d1d398fc855f11077be53582a48b2293c2fe2e85
punamthapa <punam.chhetri.372@gmail.com>
2023-08-03 17:06:28 +0545

    Python basic learning

---
+++ learning Python for test/Basics/Variables.py
@@ -0,0 +1,59 @@
+Var = "punam"
+print(Var)
+
+a = 1
+b = 2
+c = 5
+d = 6
+e = 7
+f = 8
+
+print(a, b, c)
+print(d, e, f)
+
+# Variables Assignment in Python
+
+# An integer assignment
+age = 45
+
+# A floating point
+salary = 1456.8
+
+# A string
+name = "John"
+
+print(age)
+print(salary)
+print(name)
+
+# Declaration and Initialization of Variables
+# declaring the var
+Number = 100
+
+# display
+print(Number)
+
+# Redeclaring variables in Python
+# declaring the var
+Number = 100
+
+# display
+print("Before declare: ", Number)
+
+# re-declare the var
+Number = 120.3
+
+print("After re-declare:", Number)
+
+# Python Assign Values to Multiple Variables
+a = b = c = 10
+
+print(a)
+print(b)
+print(c)
+
+p = a
+p = "apple"
+
+print(p)
+
