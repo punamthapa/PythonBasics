59eec05d132a41cc88dab25f22af2f0887eaa1ec
punamthapa <punam.chhetri.372@gmail.com>
2023-08-04 11:24:08 +0545

    datatypes

---
+++ learning Python for test/Basics/dataTypes.py
@@ -0,0 +1,50 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = 1j
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
