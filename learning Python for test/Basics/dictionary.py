8adefef905c9ea66a7bb097062da40198f1b24a1
punamthapa <punam.chhetri.372@gmail.com>
2023-08-07 22:38:08 +0545

    dictionary in python

---
+++ learning Python for test/Basics/dictionary.py
@@ -0,0 +1,11 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(thisdict["brand"])
+print(len(thisdict))
+print(type(thisdict))
+x = thisdict["model"]
+print(x)
